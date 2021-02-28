875aa5c76002128b6d03b1104f27c2b2891da7a7
藤田優作 <marozaemon1217@gmail.com>
2021-02-28 16:37:51 +0900

    main画面のUI実装

--- Friend-Collection/Controller/ViewController.swift
+++ Friend-Collection/Controller/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Friend Collection"
+        self.navigationController?.navigationBar.barTintColor = .green
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")        // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as? TableViewCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    
+    
+    
 }
 
---
+++ Friend-Collection/View/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Friend-Collection
+//
+//  Created by 藤田優作 on 2021/02/28.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
